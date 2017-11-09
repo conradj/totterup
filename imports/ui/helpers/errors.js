87fbbb47cbd67c015499ad4373612913a475044f
conradj <conradsjackson@gmail.com>
2017-11-09 08:57:45 +0000

    show all errors for now

--- imports/ui/helpers/errors.js
+++ imports/ui/helpers/errors.js
@@ -6,7 +6,7 @@ export const displayError = (error) => {
   if (error) {
     // It would be better to not alert the error here but inform the user in some
     // more subtle way
-    console.log(i18n.__(error.error)); // eslint-disable-line no-alert
+    console.log(i18n.__(error.error), error.error, error); // eslint-disable-line no-alert
 
     if (error.error === 'validation-error') {
       console.dir(error, 'error ')
