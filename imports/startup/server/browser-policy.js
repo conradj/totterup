10d182290a23748739df605584f6e3b14125a29c
conradj <conradsjackson@gmail.com>
2017-12-03 21:05:27 +0000

    allow stock photos

--- imports/startup/server/browser-policy.js
+++ imports/startup/server/browser-policy.js
@@ -1,4 +1,6 @@
-import { BrowserPolicy } from 'meteor/browser-policy-common';
-BrowserPolicy.content.allowOriginForAll('*.google-analytics.com');
-BrowserPolicy.content.allowOriginForAll( 'fonts.gstatic.com' );
+import { BrowserPolicy } from "meteor/browser-policy-common";
+BrowserPolicy.content.allowOriginForAll("*.google-analytics.com");
+BrowserPolicy.content.allowOriginForAll("fonts.gstatic.com");
 BrowserPolicy.content.allowImageOrigin("lh3.googleusercontent.com");
+BrowserPolicy.content.allowImageOrigin("images.unsplash.com");
+BrowserPolicy.content.allowImageOrigin("source.unsplash.com");
