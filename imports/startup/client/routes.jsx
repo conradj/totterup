6ec3b4585b03f9d54ff6d951b9fa9ded034729ed
conradj <conradsjackson@gmail.com>
2017-12-03 21:09:06 +0000

    auth check on router

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -1,31 +1,61 @@
-import React from 'react';
-import { Router, Route, browserHistory } from 'react-router';
-import i18n from 'meteor/universe:i18n';
+import React from "react";
+import { Router, Route, browserHistory } from "react-router";
 
+import i18n from "meteor/universe:i18n";
 // route components
-import AppContainer from '../../ui/containers/AppContainer.jsx';
-import PlayerPageContainer from '../../ui/containers/PlayerPageContainer.jsx';
-import LeaguePageContainer from '../../ui/containers/LeaguePageContainer.jsx';
-import GamePageContainer from '../../ui/containers/GamePageContainer.jsx';
-import AuthPageSignIn from '../../ui/pages/AuthPageSignIn.jsx';
-import AuthPageJoin from '../../ui/pages/AuthPageJoin.jsx';
-import AuthPageForgotPassword from '../../ui/pages/AuthPageForgotPassword.jsx';
-import NotFoundPage from '../../ui/pages/NotFoundPage.jsx';
+import AppContainer from "../../ui/containers/AppContainer.jsx";
+import PlayerPageContainer from "../../ui/containers/PlayerPageContainer.jsx";
+import LeaguePageContainer from "../../ui/containers/LeaguePageContainer.jsx";
+import GamePageContainer from "../../ui/containers/GamePageContainer.jsx";
+import AuthPageSignIn from "../../ui/pages/AuthPageSignIn.jsx";
+import AuthPageJoin from "../../ui/pages/AuthPageJoin.jsx";
+import LandingPage from "../../ui/pages/LandingPage.jsx";
+import AuthPageForgotPassword from "../../ui/pages/AuthPageForgotPassword.jsx";
+import NotFoundPage from "../../ui/pages/NotFoundPage.jsx";
 
-i18n.setLocale('en');
+i18n.setLocale("en");
+
+function requireAuth(nextState, replace) {
+  if (!Meteor.userId()) {
+    replace({
+      pathname: "/hi"
+    });
+  }
+}
 
 export const renderRoutes = () => (
   <Router history={browserHistory}>
-    <Route path="/" component={AppContainer}>
-      <Route path="leagues/:id" component={LeaguePageContainer} />
-      <Route path="leagues/:id/players" component={LeaguePageContainer} />
-      <Route path="leagues/:leagueId/players/:playerId" component={PlayerPageContainer} />
-      <Route path="leagues/:id/game" component={GamePageContainer} />
-      <Route path="games/:id" component={GamePageContainer} />
-      <Route path="signin" component={AuthPageSignIn} />
-      <Route path="join" component={AuthPageJoin} />
-      <Route path="forgotpassword" component={AuthPageForgotPassword} />
-      <Route path="*" component={NotFoundPage} />
+    <Route path="hi" component={LandingPage} />
+    <Route path="/" component={AppContainer} onEnter={requireAuth}>
+      <Route
+        path="leagues/:id"
+        component={LeaguePageContainer}
+        onEnter={requireAuth}
+      />
+      <Route
+        path="leagues/:id/players"
+        component={LeaguePageContainer}
+        onEnter={requireAuth}
+      />
+      <Route
+        path="leagues/:leagueId/players/:playerId"
+        component={PlayerPageContainer}
+        onEnter={requireAuth}
+      />
+      <Route
+        path="leagues/:id/game"
+        component={GamePageContainer}
+        onEnter={requireAuth}
+      />
+      <Route
+        path="games/:id"
+        component={GamePageContainer}
+        onEnter={requireAuth}
+      />
     </Route>
+    <Route path="signin" component={AuthPageSignIn} />
+    <Route path="join" component={AuthPageJoin} />
+    <Route path="forgotpassword" component={AuthPageForgotPassword} />
+    <Route path="*" component={NotFoundPage} />
   </Router>
 );
