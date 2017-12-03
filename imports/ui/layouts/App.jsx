d613bba9ab59a67080d381cfacad300c2e8fc9d2
conradj <conradsjackson@gmail.com>
2017-12-03 21:07:16 +0000

    welcome message

--- imports/ui/layouts/App.jsx
+++ imports/ui/layouts/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-import { Meteor } from 'meteor/meteor';
-import { Session } from 'meteor/session'; // XXX: SESSION
-import { Leagues } from '../../api/leagues/leagues.js';
-import UserMenu from '../components/UserMenu.jsx';
-import LeagueList from '../components/LeagueList.jsx';
-import LanguageToggle from '../components/LanguageToggle.jsx';
-import ConnectionNotification from '../components/ConnectionNotification.jsx';
-import Loading from '../components/Loading.jsx';
-import Logo from '../components/Logo.jsx';
+import React from "react";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
+import { Meteor } from "meteor/meteor";
+import { Session } from "meteor/session"; // XXX: SESSION
+import { Leagues } from "../../api/leagues/leagues.js";
+import UserMenu from "../components/UserMenu.jsx";
+import LeagueList from "../components/LeagueList.jsx";
+import LanguageToggle from "../components/LanguageToggle.jsx";
+import ConnectionNotification from "../components/ConnectionNotification.jsx";
+import Loading from "../components/Loading.jsx";
+import Logo from "../components/Logo.jsx";
 
 const CONNECTION_ISSUE_TIMEOUT = 5000;
 
@@ -17,7 +17,7 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       menuOpen: false,
-      showConnectionIssue: false,
+      showConnectionIssue: false
     };
     this.toggleMenu = this.toggleMenu.bind(this);
     this.logout = this.logout.bind(this);
@@ -31,28 +31,23 @@ export default class App extends React.Component {
   }
 
   componentWillReceiveProps({ loading, children }) {
-    // redirect / to a list once lists are ready
+    // // redirect / to a list once lists are ready
     if (!loading && !children) {
       const league = Leagues.findOne();
-      this.context.router.replace(`/leagues/${league._id}`);
+      if (league) {
+        this.context.router.replace(`/leagues/${league._id}`);
+      } else {
+        Session.set("menuOpen", true);
+      }
     }
   }
 
-  toggleMenu(menuOpen = !Session.get('menuOpen')) {
+  toggleMenu(menuOpen = !Session.get("menuOpen")) {
     Session.set({ menuOpen });
   }
 
   logout() {
-    Meteor.logout();
-
-    // if we are on a private league, we'll need to go to a public one
-    if (this.props.params.id) {
-      const league = Leagues.findOne(this.props.params.id);
-      if (league.userId) {
-        const publicLeague = Leagues.findOne({ userId: { $exists: false } });
-        this.context.router.push(`/leagues/${publicLeague._id}`);
-      }
-    }
+    Meteor.logout(() => this.context.router.replace("/signin"));
   }
 
   render() {
@@ -64,29 +59,28 @@ export default class App extends React.Component {
       leagues,
       menuOpen,
       children,
-      location,
+      location
     } = this.props;
 
     // eslint-disable-next-line react/jsx-no-bind
     const closeMenu = this.toggleMenu.bind(this, false);
-
     // clone route components with keys so that they can
     // have transitions
-    const clonedChildren = children && React.cloneElement(children, {
-      key: location.pathname,
-    });
+    const clonedChildren =
+      children &&
+      React.cloneElement(children, {
+        key: location.pathname
+      });
 
     return (
-      <div id="container" className={menuOpen ? 'menu-open' : ''}>
+      <div id="container" className={menuOpen ? "menu-open" : ""}>
         <section id="menu">
           <Logo />
           <UserMenu user={user} logout={this.logout} />
           <LanguageToggle />
           <LeagueList leagues={leagues} />
         </section>
-        {showConnectionIssue && !connected
-          ? <ConnectionNotification />
-          : null}
+        {showConnectionIssue && !connected ? <ConnectionNotification /> : null}
         <div className="content-overlay" onClick={closeMenu} />
         <div id="content-container">
           <ReactCSSTransitionGroup
@@ -94,9 +88,12 @@ export default class App extends React.Component {
             transitionEnterTimeout={200}
             transitionLeaveTimeout={200}
           >
-            {loading
-              ? <Loading key="loading" />
-              : clonedChildren}
+            {!loading ? clonedChildren : null}
+            <div className="welcome">
+              <h1>Welcome to Lll!</h1>
+              <span>Click + New League to make your first league</span>
+              <Logo />
+            </div>
           </ReactCSSTransitionGroup>
         </div>
       </div>
@@ -105,16 +102,16 @@ export default class App extends React.Component {
 }
 
 App.propTypes = {
-  user: React.PropTypes.object,      // current meteor user
-  connected: React.PropTypes.bool,   // server connection status
-  loading: React.PropTypes.bool,     // subscription status
-  menuOpen: React.PropTypes.bool,    // is side menu open?
-  leagues: React.PropTypes.array,      // all leagues visible to the current user
+  user: React.PropTypes.object, // current meteor user
+  connected: React.PropTypes.bool, // server connection status
+  loading: React.PropTypes.bool, // subscription status
+  menuOpen: React.PropTypes.bool, // is side menu open?
+  leagues: React.PropTypes.array, // all leagues visible to the current user
   children: React.PropTypes.element, // matched child route component
-  location: React.PropTypes.object,  // current router location
-  params: React.PropTypes.object,    // parameters of the current route
+  location: React.PropTypes.object, // current router location
+  params: React.PropTypes.object // parameters of the current route
 };
 
 App.contextTypes = {
-  router: React.PropTypes.object,
+  router: React.PropTypes.object
 };
