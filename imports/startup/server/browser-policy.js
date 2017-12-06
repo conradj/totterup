5499075223fa97e769ddda82544f5fc44d5dc3a8
conradj <conradsjackson@gmail.com>
2017-12-06 18:39:50 +0000

    allow access to scripts in prod

--- imports/startup/server/browser-policy.js
+++ imports/startup/server/browser-policy.js
@@ -1,4 +1,6 @@
 import { BrowserPolicy } from "meteor/browser-policy-common";
+BrowserPolicy.content.allowOriginForAll("totterup.com");
+BrowserPolicy.content.allowOriginForAll("totterupapp.azurewebsites.net");
 BrowserPolicy.content.allowOriginForAll("*.google-analytics.com");
 BrowserPolicy.content.allowOriginForAll("fonts.gstatic.com");
 BrowserPolicy.content.allowImageOrigin("lh3.googleusercontent.com");
