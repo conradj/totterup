fc1d342ef8a01e8c8363ab103bab2b7b2e83cccd
conradj <conradsjackson@gmail.com>
2017-12-07 06:41:51 +0000

    leagues.in publication

--- imports/api/leagues/server/publications.js
+++ imports/api/leagues/server/publications.js
@@ -1,22 +1,46 @@
 /* eslint-disable prefer-arrow-callback */
 
-import { Meteor } from 'meteor/meteor';
+import { Meteor } from "meteor/meteor";
 
-import { Leagues } from '../leagues.js';
+import { Leagues } from "../leagues.js";
 
-Meteor.publishComposite('leagues.owned', function leaguesPublic() {
+Meteor.publishComposite("leagues.owned", function leaguesPublic() {
   const userId = this.userId;
 
   return {
     find() {
-      return Meteor.users.find({_id: userId}, {fields: { ownedLeagues: 1 }});
+      return Meteor.users.find(
+        { _id: userId },
+        { fields: { ownedLeagues: 1 } }
+      );
     },
-    children: [{
-      find(user) {
-        return Leagues.find({
-          _id : { $in : user.ownedLeagues }
-        })
+    children: [
+      {
+        find(user) {
+          return Leagues.find({
+            _id: { $in: user.ownedLeagues }
+          });
+        }
       }
-    }]
-  }
+    ]
+  };
+});
+
+Meteor.publishComposite("leagues.in", function leaguesIn() {
+  const userId = this.userId;
+
+  return {
+    find() {
+      return Meteor.users.find({ _id: userId }, { fields: { inLeagues: 1 } });
+    },
+    children: [
+      {
+        find(user) {
+          return Leagues.find({
+            _id: { $in: user.inLeagues }
+          });
+        }
+      }
+    ]
+  };
 });
