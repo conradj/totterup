8343359fd3b3db53ef93a36afc9d17197159f401
conradj <conradsjackson@gmail.com>
2017-12-09 21:45:21 +0000

    linted

--- imports/ui/containers/PlayerPageContainer.jsx
+++ imports/ui/containers/PlayerPageContainer.jsx
@@ -1,17 +1,19 @@
-import { Meteor } from 'meteor/meteor';
-import React from 'react';
-import { Players } from '../../api/players/players.js';
-import PlayerPage from '../pages/PlayerPage.jsx';
-import { withTracker } from 'meteor/react-meteor-data';
+import { Meteor } from "meteor/meteor";
+import React from "react";
+import { Players } from "../../api/players/players.js";
+import PlayerPage from "../pages/PlayerPage.jsx";
+import { withTracker } from "meteor/react-meteor-data";
 
-export default PlayerPageContainer = withTracker(({ params: { leagueId, playerId } }) => {
-  const playersHandle = Meteor.subscribe('players');
-  const loading = !playersHandle.ready()
-  const player = Players.findOne(playerId);
-  const playerExists = !loading && !!player;
-  return {
-    player,
-    playerExists,
-    loading
-  };
-}) (PlayerPage);
+export default (PlayerPageContainer = withTracker(
+  ({ params: { leagueId, playerId } }) => {
+    const playersHandle = Meteor.subscribe("players");
+    const loading = !playersHandle.ready();
+    const player = Players.findOne(playerId);
+    const playerExists = !loading && !!player;
+    return {
+      player,
+      playerExists,
+      loading
+    };
+  }
+)(PlayerPage));
