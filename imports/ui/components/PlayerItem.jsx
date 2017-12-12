eeff319b84ca8a005fed3a1fafd42d48326cf16d
conradj <conradsjackson@gmail.com>
2017-12-12 19:54:37 +0000

    use the league colour for players w/out avatars

--- imports/ui/components/PlayerItem.jsx
+++ imports/ui/components/PlayerItem.jsx
@@ -28,11 +28,16 @@ export default class PlayerItem extends BaseComponent {
   render() {
     const { player, editing, score, linkUrl, position } = this.props;
     const positionClassName = `player-container position${position}`;
-    const backgroundStyle = { backgroundImage: `url(${player.avatar})` };
+    const backgroundStyle = {
+      backgroundImage: `url(${player.avatar})`,
+      backgroundColor: `#${player.league().inviteCode}`
+    };
     const playerComponent = (
       <div className="player-item">
         <div className={positionClassName}>
-          <div className="player-image" style={backgroundStyle} />
+          <div className="player-image" style={backgroundStyle}>
+            <div>+</div>
+          </div>
           <div className="player-position">
             <span>
               {position}
