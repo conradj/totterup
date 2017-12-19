81f6bc13b75e38c0db30b06bf1fa011f8780bc3e
conradj <conradsjackson@gmail.com>
2017-12-19 22:29:32 +0000

    tidy loading pages

--- imports/ui/components/Loading.jsx
+++ imports/ui/components/Loading.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import i18n from 'meteor/universe:i18n';
-import BaseComponent from './BaseComponent.jsx';
-import Logo from './Logo.jsx';
+import React from "react";
+import i18n from "meteor/universe:i18n";
+import BaseComponent from "./BaseComponent.jsx";
+import Logo from "./Logo.jsx";
 
 class Loading extends BaseComponent {
   render() {
     return (
-      <Logo />
+      <div className="loading-app">
+        <h2>Loading...</h2>
+        <Logo showTitle={false} showStrapline={false} />
+      </div>
     );
   }
 }
