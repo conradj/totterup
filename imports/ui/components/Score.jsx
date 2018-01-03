548922e2acd4ce13f906feb2e4d5cfd2e1af703b
conradj <conradsjackson@gmail.com>
2018-01-03 22:57:47 +0000

    use league max score for slider

--- imports/ui/components/Score.jsx
+++ imports/ui/components/Score.jsx
@@ -1,15 +1,12 @@
 /* global alert */
 
-import React from 'react';
-import { Link } from 'react-router';
-import i18n from 'meteor/universe:i18n';
-import BaseComponent from './BaseComponent.jsx';
-import { displayError } from '../helpers/errors.js';
+import React from "react";
+import { Link } from "react-router";
+import i18n from "meteor/universe:i18n";
+import BaseComponent from "./BaseComponent.jsx";
+import { displayError } from "../helpers/errors.js";
 import Message from "../components/Message.jsx";
-import {
-  updateScore
-} from '../../api/scores/methods.js';
-
+import { updateScore } from "../../api/scores/methods.js";
 
 export default class Score extends BaseComponent {
   constructor(props) {
@@ -35,39 +32,52 @@ export default class Score extends BaseComponent {
 
   saveScore() {
     const { score } = this.props;
-    updateScore.call({
-      scoreId: score._id,
-      newScore: parseInt(this.scoreInput.value),
-    }, displayError);
+    updateScore.call(
+      {
+        scoreId: score._id,
+        newScore: parseInt(this.scoreInput.value)
+      },
+      displayError
+    );
   }
 
   render() {
     const { score } = this.props;
     const player = score.player();
-    const backgroundStyle = {backgroundImage: `url(${player.avatar})`};
+    const league = score.league();
+    const backgroundStyle = { backgroundImage: `url(${player.avatar})` };
     const sliderImageClassName = `score-slider-${player.text}`;
     const sliderClassNames = `score-slider ${sliderImageClassName}`;
     return (
-      <form className="league-edit-form" onSubmit={this.onScoreFormSubmit}>    
+      <form className="league-edit-form" onSubmit={this.onScoreFormSubmit}>
         <div className="score-container list-item">
-          <div className="score-player-name">{ player.text } {score.score} {score.score == 1 ? `point` : `points`}</div>
+          <div className="score-player-name">
+            {player.text} {score.score} {score.score == 1 ? `point` : `points`}
+          </div>
           <div className="score-player-score-slider">
-          <style dangerouslySetInnerHTML={{__html: `
+            <style
+              dangerouslySetInnerHTML={{
+                __html: `
             input[type=range].${sliderImageClassName}::-webkit-slider-thumb
             { background-image: url(${player.avatar}) }
 
             input[type=range].${sliderImageClassName}::-moz-range-thumb
             { background-image: url(${player.avatar}) }
-          `}} />
-              <input type="range"
-                className={sliderClassNames} 
-                max="4"
-                defaultValue={score.score}
-                ref={(c) => { this.scoreInput = c; }}
-                name="score"
-                onBlur={this.onScoreInputBlur}
-                onChange={this.onScoreInputChange}
-                />
+          `
+              }}
+            />
+            <input
+              type="range"
+              className={sliderClassNames}
+              max={league.maxScore}
+              defaultValue={score.score}
+              ref={c => {
+                this.scoreInput = c;
+              }}
+              name="score"
+              onBlur={this.onScoreInputBlur}
+              onChange={this.onScoreInputChange}
+            />
           </div>
         </div>
       </form>
@@ -76,9 +86,9 @@ export default class Score extends BaseComponent {
 }
 
 Score.propTypes = {
-  score: React.PropTypes.object,
+  score: React.PropTypes.object
 };
 
 Score.contextTypes = {
-  router: React.PropTypes.object,
+  router: React.PropTypes.object
 };
