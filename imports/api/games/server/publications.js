d02fb30fc27ff15ede6739e79b0afd064442acea
conradj <conradsjackson@gmail.com>
2018-01-07 15:03:23 +0000

    allow all players to see games in same league

--- imports/api/games/server/publications.js
+++ imports/api/games/server/publications.js
@@ -1,21 +1,38 @@
 /* eslint-disable prefer-arrow-callback */
-import { Meteor } from 'meteor/meteor';
-import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+import { Meteor } from "meteor/meteor";
+import { SimpleSchema } from "meteor/aldeed:simple-schema";
 
-import { Games } from '../games.js';
-import { Leagues } from '../../leagues/leagues.js';
+import { Games } from "../games.js";
+import { Leagues } from "../../leagues/leagues.js";
 
-Meteor.publishComposite('games', function games() {
+Meteor.publishComposite("games", function games() {
   const userId = this.userId;
 
   return {
     find() {
-      return Meteor.users.find({_id: userId}, { fields: { ownedLeagues: 1 }});
+      return Meteor.users.find(
+        { _id: userId },
+        { fields: { ownedLeagues: 1, inLeagues: 1 } }
+      );
     },
-    children: [{
-      find(user) {
-        return Games.find({leagueId : { $in : user.ownedLeagues }}, { fields: Games.publicFields });  
+    children: [
+      {
+        find(user) {
+          return Games.find(
+            {
+              $or: [
+                {
+                  leagueId: { $in: user.ownedLeagues }
+                },
+                {
+                  leagueId: { $in: user.inLeagues }
+                }
+              ]
+            },
+            { fields: Games.publicFields }
+          );
+        }
       }
-    }]
+    ]
   };
 });
